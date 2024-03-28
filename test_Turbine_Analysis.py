84824ee27fac9ea7ae5bbbbe867ef46e81a9f8ba
tschmdt <e12112219@student.tuwien.ac.at>
2024-03-28 13:52:15 +0100

    import math

--- test_Turbine_Analysis.py
+++ test_Turbine_Analysis.py
@@ -1,6 +1,6 @@
 # test function for the "numbers_of_turbines" function
 # since the model turbine has an output of 3 MW, an installed capacity below 3 MW should still return 1 turbine
-
+import math
 def test_number_of_turbines():
     assert number_of_turbines(1) == 1
     assert number_of_turbines(2) == 1
