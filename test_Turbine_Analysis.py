66d4fa513ef0981e77808a9678279cfab8a9a3a7
tschmdt <e12112219@student.tuwien.ac.at>
2024-03-28 14:08:13 +0100

    add the missing import statement to test file

--- test_Turbine_Analysis.py
+++ test_Turbine_Analysis.py
@@ -1,6 +1,8 @@
+from Turbine_Analysis import number_of_turbines, energy_output
+
 # test function for the "numbers_of_turbines" function
 # since the model turbine has an output of 3 MW, an installed capacity below 3 MW should still return 1 turbine
-import math
+
 def test_number_of_turbines():
     assert number_of_turbines(1) == 1
     assert number_of_turbines(2) == 1
