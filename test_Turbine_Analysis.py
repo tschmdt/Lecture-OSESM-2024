faec8ae54f1a17e021a1154658805bf8eddab4b6
tschmdt <e12112219@student.tuwien.ac.at>
2024-03-28 13:45:13 +0100

    add a test function for energy_output function

--- test_Turbine_Analysis.py
+++ test_Turbine_Analysis.py
@@ -8,3 +8,12 @@ def test_number_of_turbines():
     assert number_of_turbines(5) == 2
     assert number_of_turbines(100) == 34
 
+# test function for the "energy_output" function
+# Unit: TWh
+def test_energy_output():
+    assert energy_output(3, 6, 2500) == 0.017
+    assert energy_output(1, 6, 200) == 0.013
+    assert energy_output(100, 6, 2500) == 1.357
+    assert energy_output(250, 9, 2400) == 4.581
+    assert energy_output(600, 6, 2000) == 2.693
+
