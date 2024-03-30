60c33fad4e5d7ab7019d7607ffddbb3496507715
tschmdt <e12112219@student.tuwien.ac.at>
2024-03-30 12:01:54 +0100

    adapt comments in code

--- Turbine_Analysis.py
+++ Turbine_Analysis.py
@@ -1,6 +1,5 @@
 import math
 
-
 # function that returns the number of turbines that need to be installed for a desired capacity (input)
 # a standard wind turbine with an output of 3 MW serves as the basis (Vestas V90 3 MW Onshore)
 def number_of_turbines(installed_capacity):
@@ -12,7 +11,7 @@ def number_of_turbines(installed_capacity):
 # variable parameters are installed_capacity, average_wind_speed and full_load_hours
 # Units: installed_capacity in MW, average_wind_speed in m/s, full_load_hours in hours, lifetime in years, rotor_diameter in m,
 # wind_density in kg/m^3, energy_output in TWh
-# Base model turbine vestas V90 3 MW Onshore
+# Base model turbine Vestas V90 3 MW Onshore
 
 wind_density = 1.225  # kg/m^3
 rotor_diameter = 90  # m
--- test_Turbine_Analysis.py
+++ test_Turbine_Analysis.py
@@ -3,19 +3,18 @@ from Turbine_Analysis import number_of_turbines, energy_output
 # test function for the "numbers_of_turbines" function
 # since the model turbine has an output of 3 MW, an installed capacity below 3 MW should still return 1 turbine
 
-
 def test_number_of_turbines():
     assert number_of_turbines(1) == 1
     assert number_of_turbines(2) == 1
     assert number_of_turbines(3) == 1
     assert number_of_turbines(5) == 2
-    assert number_of_turbines(100) == 30 #34
+    assert number_of_turbines(100) == 34
 
 
 # test function for the "energy_output" function
 # Unit: TWh
 def test_energy_output():
-    assert energy_output(3, 6, 2500) == 0.01 #0.017
+    assert energy_output(3, 6, 2500) == 0.017
     assert energy_output(1, 6, 2000) == 0.013
     assert energy_output(100, 6, 2500) == 0.572
     assert energy_output(250, 9, 2400) == 4.581
